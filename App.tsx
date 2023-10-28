0e44d372207f6306862eac7d3d6e6b44e640659e
Amir Angel <36531255+17Amir17@users.noreply.github.com>
2023-10-28 20:39:46 +0300

    reprod

--- App.tsx
+++ App.tsx
@@ -1,66 +1,89 @@
-import React from 'react';
-import {Button, Text, View} from 'react-native';
+import React, {RefObject, useCallback, useMemo, useRef} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-const Stack = createNativeStackNavigator();
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {BottomSheetModal, BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 
 function App(): JSX.Element {
+  const safeAreaModal = useRef<BottomSheetModal>(null);
+  const regularModal = useRef<BottomSheetModal>(null);
+
+  const showSafeAreaModal = useCallback(() => {
+    safeAreaModal.current?.present();
+  }, []);
+
+  const showRegularModal = useCallback(() => {
+    regularModal.current?.present();
+  }, []);
+
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="RegularView" component={RegularView} />
-          <Stack.Screen name="ViewWithSafeArea" component={ViewWithSafeArea} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <BottomSheetModalProvider>
+        <SafeAreaProvider>
+          <View
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              gap: 12,
+            }}>
+            <Text>Home</Text>
+            <Button
+              onPress={showSafeAreaModal}
+              title="Bottom Sheet With Safe Area"
+            />
+            <Button
+              onPress={showRegularModal}
+              title="Bottom Sheet Without Safe Area"
+            />
+          </View>
+          <BottomSheetWithSafeArea bottomRef={safeAreaModal} />
+          <BottomSheetWithoutSafeArea bottomRef={regularModal} />
+        </SafeAreaProvider>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 }
 
-function HomeScreen({navigation}) {
+function BottomSheetWithoutSafeArea({
+  bottomRef,
+}: {
+  bottomRef: RefObject<BottomSheetModal>;
+}) {
+  const snapPoints = useMemo(() => ['25%', '50%'], []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: 12,
-      }}>
-      <Text>Home Screen</Text>
-      <Button
-        onPress={() => {
-          navigation.navigate('RegularView');
-        }}
-        title="Navigate To Regular View"
-      />
-      <Button
-        onPress={() => {
-          navigation.navigate('ViewWithSafeArea');
-        }}
-        title="Navigate To View With Safe Area"
-      />
-    </View>
+    <BottomSheetModal ref={bottomRef} index={1} snapPoints={snapPoints}>
+      <View style={styles.container}>
+        <Text>Without Safe Area</Text>
+      </View>
+    </BottomSheetModal>
   );
 }
 
-function RegularView() {
+function BottomSheetWithSafeArea({
+  bottomRef,
+}: {
+  bottomRef: RefObject<BottomSheetModal>;
+}) {
+  const snapPoints = useMemo(() => ['25%', '50%'], []);
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Regular View</Text>
-    </View>
+    <BottomSheetModal ref={bottomRef} index={1} snapPoints={snapPoints}>
+      <SafeAreaView style={styles.container}>
+        <Text>With Safe Area</Text>
+      </SafeAreaView>
+    </BottomSheetModal>
   );
 }
 
-function ViewWithSafeArea() {
-  return (
-    <SafeAreaView
-      style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>View with Safe Area</Text>
-    </SafeAreaView>
-  );
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    backgroundColor: 'grey',
+  },
+});
 
 export default App;
